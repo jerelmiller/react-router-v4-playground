0d2890e0582586e329bd7648349010a41bb9492f
Jerel Miller <jerelmiller@gmail.com>
2016-12-07 23:59:10 -0800

    Wrap App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { BrowserRouter } from 'react-router'
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () =>
-  <div className='App'>
-    <div className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <h2>Welcome to React</h2>
+  <BrowserRouter>
+    <div className='App'>
+      <div className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className='App-intro'>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
-    <p className='App-intro'>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  </BrowserRouter>
 
 export default App
