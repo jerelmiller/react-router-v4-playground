3ad1a1aef07b6665264e40f2da523a2656c31a17
Jerel Miller <jerelmiller@gmail.com>
2016-12-08 00:01:44 -0800

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter } from 'react-router'
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const App = () =>
   <BrowserRouter>
     <div className='App'>
       <div className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <h2>Welcome to React</h2>
       </div>
       <p className='App-intro'>
